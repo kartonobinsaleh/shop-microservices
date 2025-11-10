b0b6e786142713ce93eb6a2763da9d2b9843f173
kartonobinsaleh <kartono.saleh@gmail.com>
2025-11-11 05:11:00 +0700

    feat(orders): add controller order

---
+++ order-service/src/main/java/com/kartonobinsaleh/microservices/order_service/controller/OrderController.java
@@ -0,0 +1,30 @@
+package com.kartonobinsaleh.microservices.order_service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kartonobinsaleh.microservices.order_service.dto.OrderRequest;
+import com.kartonobinsaleh.microservices.order_service.service.OrderService;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/api/orders")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        orderService.placeOrder(orderRequest);
+        return "Order Placed Succesfully";
+    }
+
+}
