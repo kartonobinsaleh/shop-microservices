ee7838457a993b1e7a730b0b0ae22fa1ad6484b8
kartonobinsaleh <kartono.saleh@gmail.com>
2025-11-15 12:51:55 +0700

    feat(inventory): add inventory repository

---
+++ inventory-service/src/main/java/com/kartonobinsaleh/microservice/inventory_service/repository/InventoryRespository.java
@@ -0,0 +1,9 @@
+package com.kartonobinsaleh.microservice.inventory_service.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kartonobinsaleh.microservice.inventory_service.model.Inventory;
+
+public interface InventoryRespository extends JpaRepository<Inventory, Long> {
+
+}
