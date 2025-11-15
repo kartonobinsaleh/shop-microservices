a5ef2878802fe28e9248081e64457c849afac070
kartonobinsaleh <kartono.saleh@gmail.com>
2025-11-15 21:03:46 +0700

    feat(test): Add WireMock stub for InventoryClient in OrderService tests

--- order-service/src/test/java/com/kartonobinsaleh/microservices/order_service/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/kartonobinsaleh/microservices/order_service/OrderServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package com.kartonobinsaleh.microservices.order_service;
 
+import com.kartonobinsaleh.microservices.order_service.stubs.InventoryClientStub;
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,12 +8,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.context.annotation.Import;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.testcontainers.containers.MySQLContainer;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
 	@ServiceConnection
@@ -34,17 +36,18 @@ class OrderServiceApplicationTests {
 	void shouldSubmitOrder() {
 		String submitOrderJson = """
 				{
-				     "skuCode": "iphone 12",
+				     "skuCode": "iphone_15",
 				     "price": 1000,
 				     "quantity": 1
 				}
 				""";
 
+		InventoryClientStub.stubInventoryCall("iphone_15", 1);
 		var responseBodyString = RestAssured.given()
 				.contentType("application/json")
 				.body(submitOrderJson)
 				.when()
-				.post("/api/orders")
+				.post("/api/order")
 				.then()
 				.log().all()
 				.statusCode(201)
