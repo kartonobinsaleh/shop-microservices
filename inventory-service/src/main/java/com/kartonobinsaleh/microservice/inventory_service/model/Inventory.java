6843cdd2290f36b2a29077bffe6f45e980a0b70f
kartonobinsaleh <kartono.saleh@gmail.com>
2025-11-15 12:51:33 +0700

    feat(inventory): add inventory model

---
+++ inventory-service/src/main/java/com/kartonobinsaleh/microservice/inventory_service/model/Inventory.java
@@ -0,0 +1,18 @@
+package com.kartonobinsaleh.microservice.inventory_service.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "inventory")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Inventory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String skuCode;
+    private Integer quantity;
+}
