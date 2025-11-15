04bd9ce2d8eaecef9a4263c0800771d81f093ab5
kartonobinsaleh <kartono.saleh@gmail.com>
2025-11-15 19:50:04 +0700

    chore(inventory): add existsBySkuCodeAndQuantityIsGreaterThanEquals

--- inventory-service/src/main/java/com/kartonobinsaleh/microservice/inventory_service/repository/InventoryRespository.java
+++ inventory-service/src/main/java/com/kartonobinsaleh/microservice/inventory_service/repository/InventoryRespository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.kartonobinsaleh.microservice.inventory_service.model.Inventory;
 
 public interface InventoryRespository extends JpaRepository<Inventory, Long> {
-
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEquals(String skuCode, Integer quantity);
 }
