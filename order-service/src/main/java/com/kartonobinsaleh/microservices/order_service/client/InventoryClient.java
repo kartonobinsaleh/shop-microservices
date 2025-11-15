f99e793525fcd4a4b587207ef1cb31a28e4408a1
kartonobinsaleh <kartono.saleh@gmail.com>
2025-11-15 20:50:09 +0700

    feat(orders): integrate OrderService with Inventory via Feign client

---
+++ order-service/src/main/java/com/kartonobinsaleh/microservices/order_service/client/InventoryClient.java
@@ -0,0 +1,12 @@
+package com.kartonobinsaleh.microservices.order_service.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "inventory", url = "http://localhost:8082")
+public interface InventoryClient {
+
+    @GetMapping(path = "/api/inventory")
+    boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+}
--- order-service/src/main/java/com/kartonobinsaleh/microservices/order_service/service/OrderService.java
+++ order-service/src/main/java/com/kartonobinsaleh/microservices/order_service/service/OrderService.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import com.kartonobinsaleh.microservices.order_service.client.InventoryClient;
 import com.kartonobinsaleh.microservices.order_service.dto.OrderRequest;
 import com.kartonobinsaleh.microservices.order_service.model.Order;
 import com.kartonobinsaleh.microservices.order_service.repository.OrderRespository;
@@ -15,15 +16,23 @@ import lombok.RequiredArgsConstructor;
 public class OrderService {
 
     private final OrderRespository orderRespository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest) {
-        Order order = new Order();
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
 
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
+        if (isProductInStock) {
+            Order order = new Order();
 
-        orderRespository.save(order);
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+
+            orderRespository.save(order);
+        } else {
+            throw new RuntimeException("Product with SkuCode" + orderRequest.skuCode() + "is not in stock");
+        }
+
     }
 }
